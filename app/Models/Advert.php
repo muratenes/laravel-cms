6d09593a2e16227587d2053243368d38aadb9774
Murat Karabacak <m9urat@gmail.com>
2021-04-04 19:46:02 +0300

    added:advert table and model

---
+++ app/Models/Advert.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Advert extends Model
+{
+    protected $guarded = ['id'];
+
+    const MODULE_NAME = 'advert';
+
+    const TYPE_HOME_1 = 'home_1';
+    const TYPE_HOME_2 = 'home_2';
+    const TYPE_HOME_3 = 'home_3';
+    const TYPE_HOME_4 = 'home_4';
+
+
+    public static function listStatusWithId()
+    {
+        return [
+            // index => [id,label,size]
+            self::TYPE_HOME_1 => [self::TYPE_HOME_1, "Anasayfa 1", "540x300"],
+            self::TYPE_HOME_2 => [self::TYPE_HOME_2, "Anasayfa 2", "540x300"],
+            self::TYPE_HOME_3 => [self::TYPE_HOME_3, "Anasayfa 3", "1140x300"],
+            self::TYPE_HOME_4 => [self::TYPE_HOME_4, "Anasayfa 4", "445x350"],
+        ];
+    }
+
+
+    public static function getHtml($type)
+    {
+        $advert = Advert::where(['status' => 1, 'type' => $type])->latest()->first();
+        return $advert ? view('site.advert.' . $advert->type, compact('advert')) : '';
+    }
+}
